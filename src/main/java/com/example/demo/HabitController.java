4a3f87211612c08af37475796c63dd169d139a8f
Nam Phan <namphn.2@gmail.com>
2025-11-10 14:01:52 +0100

    @CrossOrigin in HabitController hinzugefügt

--- src/main/java/com/example/demo/HabitController.java
+++ src/main/java/com/example/demo/HabitController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://myhabittracker-frontend.onrender.com")
 public class HabitController {
 
     // GET-Route für /habits → gibt alle Habits als JSON-Liste (JavaScript Object Notation Format) zurück
